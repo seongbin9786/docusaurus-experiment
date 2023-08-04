a0662c7df336f463d0c237e9dc2aaf06140ed4d8
Seongbin Kim <seongbin9786@gmail.com>
2023-08-04 23:16:29 +0900

    fix: Broken Link 제거

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,7 +108,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
--- src/pages/_index.tsx
+++ src/pages/_index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/">
             블로그로 이동
           </Link>
         </div>
